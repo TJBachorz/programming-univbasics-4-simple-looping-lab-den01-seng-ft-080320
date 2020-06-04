98973f5c260755f018e699a256f11423d6633fdb
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-04 18:39:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,3 +12,11 @@ def loop_message_n_times(message, n)
     puts message
   end
 end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
